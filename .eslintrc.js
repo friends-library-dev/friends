f6b00f35b84947ef9bb3966a142fa780c830054e
Jared Henderson <jared@netrivet.com>
2020-09-25 16:05:17 -0400

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins: [`@friends-library/eslint-plugin`],
+  extends: [`plugin:@friends-library/eslint-plugin/base`],
+};
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '__tests__/.*spec\\.ts$',
-  transformIgnorePatterns: ['node_modules/(?!(@friends-library)/)'],
+  preset: `ts-jest`,
+  testEnvironment: `node`,
+  testRegex: `__tests__/.*spec\\.ts$`,
+  transformIgnorePatterns: [`node_modules/(?!(@friends-library)/)`],
 };
