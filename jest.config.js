7c6543059a72433fdea3866ebc7038a379c54345
Jared Henderson <jared@netrivet.com>
2020-09-30 10:21:57 -0400

    switch to @friends-library/dev for dev scripts

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: `ts-jest`,
-  testEnvironment: `node`,
-  testRegex: `__tests__/.*spec\\.ts$`,
-  transformIgnorePatterns: [`node_modules/(?!(@friends-library)/)`],
-};
