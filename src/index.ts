fffea565e8b2e81b160a4e3917ad9d90457ae024
Jared Henderson <jaredh159@users.noreply.github.com>
2020-10-04 18:01:58 -0400

    isbns as first-class exported module (#6)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as Document } from './Document';
 export { default as Audio } from './Audio';
 export { default as AudioPart } from './AudioPart';
 export { default as friendFromJS } from './map';
+export { default as isbns } from './isbns';
 export {
   getFriend,
   getAllFriends,
