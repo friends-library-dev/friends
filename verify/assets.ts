f1c475a9b93eb2c87a6b7007fd231fbefcc2a393
Jared Henderson <jared@netrivet.com>
2020-10-08 16:41:58 -0400

    add verification scripts for ci

---
+++ verify/assets.ts
@@ -0,0 +1,83 @@
+import '@friends-library/env/load';
+import fetch from 'node-fetch';
+import pLimit from 'p-limit';
+import env from '@friends-library/env';
+import { AUDIO_QUALITIES } from '@friends-library/types';
+import * as docMeta from '@friends-library/document-meta';
+import { eachEdition } from '../';
+
+async function main(): Promise<void> {
+  const meta = await docMeta.fetch();
+  const promises: Promise<any>[] = [];
+  const result = { success: true };
+
+  eachEdition(({ edition }) => {
+    const { isDraft, audio } = edition;
+    if (isDraft) {
+      return;
+    }
+
+    promises.push(verify(edition.filepath(`web-pdf`), result));
+    promises.push(verify(edition.filepath(`mobi`), result));
+    promises.push(verify(edition.filepath(`epub`), result));
+
+    const edMeta = meta.get(edition.path);
+    if (!edMeta) {
+      console.error(`Missing edition meta for ${edition.path}`);
+      result.success = false;
+    } else if (edMeta.paperback.volumes.length > 1) {
+      edMeta.paperback.volumes.forEach((_, idx) => {
+        promises.push(verify(edition.filepath(`paperback-cover`, idx + 1), result));
+        promises.push(verify(edition.filepath(`paperback-interior`, idx + 1), result));
+      });
+    } else {
+      promises.push(verify(edition.filepath(`paperback-cover`), result));
+      promises.push(verify(edition.filepath(`paperback-interior`), result));
+    }
+
+    if (audio) {
+      promises.push(verify(audio.imagePath, result));
+      promises.push(verify(audio.zipFilepathHq, result));
+      promises.push(verify(audio.zipFilepathHq, result));
+      promises.push(verify(audio.m4bFilepathLq, result));
+      promises.push(verify(audio.m4bFilepathLq, result));
+
+      audio.parts.forEach((_, idx) => {
+        AUDIO_QUALITIES.forEach((quality) => {
+          const audioPath = audio.partFilepath(idx, quality);
+          promises.push(verify(audioPath, result));
+        });
+      });
+    }
+  });
+
+  await Promise.all(promises);
+
+  if (!result.success) {
+    process.exit(1);
+  }
+
+  console.log(`√ All cloud assets verified successfully.\n`);
+}
+
+function verify(path: string, result: { success: boolean }): Promise<void> {
+  const { CLOUD_STORAGE_BUCKET_URL: URL } = env.require(`CLOUD_STORAGE_BUCKET_URL`);
+  return limit(async () => {
+    try {
+      const res = await fetch(`${URL}/${path}`, {
+        method: `HEAD`,
+      });
+      if (res.status !== 200) {
+        console.error(`Bad status for asset ${path} : ${res.status}`);
+        result.success = false;
+      }
+    } catch (err) {
+      console.error(`Error thrown retrieving asset ${path} : ${err}`);
+      result.success = false;
+    }
+  });
+}
+
+const limit = pLimit(process.env.CI ? 10 : 100);
+
+main();
---
+++ verify/intake.ts
@@ -0,0 +1,76 @@
+import '@friends-library/env/load';
+import { query, hydrate } from '@friends-library/dpc-fs';
+
+function main(): void {
+  let err = false;
+  query.getByPattern().forEach((dpc) => {
+    hydrate.entities(dpc);
+    hydrate.asciidoc(dpc);
+    if (!dpc.edition) {
+      throw new Error(`Missing dpc edition entity`);
+    }
+
+    const signsOfIntake = dpc.asciidoc
+      .split(`\n`)
+      .reduce((count: number, line: string) => {
+        for (const regex of regexes) {
+          if (regex.test(line)) {
+            return count + 1;
+          }
+        }
+        return count;
+      }, 0);
+
+    const rate = dpc.asciidoc.split(`\n`).length / signsOfIntake;
+
+    if (rate > 650 && dpc.edition.isDraft === false) {
+      console.error(`${dpc.path}} likely not intaken`);
+      err = true;
+    }
+  });
+
+  if (err) {
+    process.exit(1);
+  }
+
+  console.log(`√ All editions complete or correctly marked as draft.\n`);
+}
+
+const regexes = [
+  /^\[quote]$/,
+  /^\[quote\.scripture.*]$/,
+  /^\[quote\.section-epigraph.*]$/,
+  /^____$/,
+  /style-blurb/,
+  /chapter-subtitle--blurb/,
+  /short="/,
+  /chapter-synopsis/,
+  /\.alt\b/,
+  /\.old-style/,
+  /\.blurb/,
+  /^\[\.asterism\]$/,
+  /^\[\.small-break\]$/,
+  /^'''$/,
+  /\.centered/,
+  /^__Objection:__$/,
+  /\.offset\b/,
+  /\.salutation/,
+  /\.signed-section/,
+  /\.embedded-content-document/,
+  /\.postscript/,
+  /\.book-title/,
+  /\.underline/,
+  /\.section-author/,
+  /\.section-date/,
+  /\.heading-continuation-blurb/,
+  /\.section-summary-preface/,
+  /\.numbered/,
+  /\.emphasized/,
+  /\.the-end/,
+  /\.syllogism/,
+  /::$/,
+  /\.discourse-part/,
+  /^\[verse.+]$/,
+];
+
+main();
