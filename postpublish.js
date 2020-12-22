89b0556b2930eb54c18948b57a41f3c3df425020
Jared Henderson <jared@netrivet.com>
2020-12-22 16:59:36 -0500

    add post-publish auto rebuild/deploy of API

---
+++ postpublish.js
@@ -0,0 +1,13 @@
+require(`@friends-library/env/load`);
+const exec = require(`x-exec`).default;
+const { magenta, gray } = require(`x-chalk`);
+const env = require(`@friends-library/env`).default;
+
+const { POST_PUBLISH_API_BUILD_HOOK_URL } = env.require(
+  `POST_PUBLISH_API_BUILD_HOOK_URL`,
+);
+
+gray(`\nWaiting 15 seconds for published package to be ready before rebuilding API...\n`);
+exec.out(`sleep 15`);
+exec.exit(`curl -X POST -d {} ${POST_PUBLISH_API_BUILD_HOOK_URL}`);
+magenta(`API rebuild triggered.\n`);
