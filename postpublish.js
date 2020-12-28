ede592fe286494243c5f895952aaea10f1eca14e
Jared Henderson <jared@netrivet.com>
2020-12-28 10:27:53 -0500

    increase deploy wait to 30 sec

--- postpublish.js
+++ postpublish.js
@@ -7,7 +7,7 @@ const { POST_PUBLISH_API_BUILD_HOOK_URL } = env.require(
   `POST_PUBLISH_API_BUILD_HOOK_URL`,
 );
 
-gray(`\nWaiting 15 seconds for published package to be ready before rebuilding API...\n`);
-exec.out(`sleep 15`);
+gray(`\nWaiting 30 seconds for published package to be ready before rebuilding API...\n`);
+exec.out(`sleep 30`);
 exec.exit(`curl -X POST -d {} ${POST_PUBLISH_API_BUILD_HOOK_URL}`);
 magenta(`API rebuild triggered.\n`);
