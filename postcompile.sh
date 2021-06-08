08d024b569c738e08d44df561477c9967ecdf20b
Jared Henderson <jared@netrivet.com>
2021-06-08 11:18:11 -0400

    changes to support new app version with e-reader

--- postcompile.sh
+++ postcompile.sh
@@ -1,4 +1,4 @@
 # because we also expose the `@friends-library/friends/query` entrypoint
 # we need to copy the typescript typings up in to the root dir modifying
-# the relative paths to TS is happy
+# the relative paths so TS is happy
 cat ./dist/query.d.ts | perl -pe "s/from '\.\//from '.\/dist\//" > ./query.d.ts
